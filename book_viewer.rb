020db77e3d1ccaee607d593669b375cc10102218
John Isom <John.Isom2001@gmail.com>
2019-11-06 10:32:34 -0700

    Add endpoint for chapter 1

--- book_viewer.rb
+++ book_viewer.rb
@@ -8,3 +8,12 @@ get '/' do
 
   erb :home
 end
+
+get '/chapters/1' do
+  @title = 'The Adventures of Sherlock Holmes'
+  @chapter_title = 'Chapter 1'
+  @contents = File.readlines('data/toc.txt', chomp: true)
+  @chapter = File.read('data/chp1.txt')
+
+  erb :chapter
+end
