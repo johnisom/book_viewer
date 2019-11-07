595f36aaeee6cf575984fbb3c15292f4d615df48
John Isom <John.Isom2001@gmail.com>
2019-11-07 14:45:46 -0700

    Move @contents to before do..end

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,15 +2,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+before do
+  @contents = File.readlines('data/toc.txt', chomp: true)
+end
+
 get '/' do
   @title = 'The Adventures of Sherlock Holmes'
-  @contents = File.readlines('data/toc.txt', chomp: true)
 
   erb :home
 end
 
 get '/chapters/:num' do |num|
-  @contents = File.readlines('data/toc.txt', chomp: true)
   @title = "Chapter #{num}: #{@contents[num.to_i - 1]}"
   @chapter = File.read("data/chp#{num}.txt")
 
